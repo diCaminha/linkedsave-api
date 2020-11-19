e4cf061277bb96750bf2e778fdfc9310651e1ddd
Denis <dineresc@gmail.com>
2020-11-19 16:13:37 -0300

    add try catch

--- src/controllers/linksController.js
+++ src/controllers/linksController.js
@@ -1,12 +1,17 @@
-const Link = require('../models/link');
+const Link = require("../models/link");
 
 exports.getLinks = async (req, res) => {
+  try {
     const links = await Link.find();
-    
     res.status(200).json({
-    message: "links fetched with success",
-    data: links,
-  });
+      message: "links fetched with success",
+      data: links,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "error trying to get the links from the db: " + err,
+    });
+  }
 };
 
 exports.saveLink = async (req, res) => {
@@ -17,12 +22,16 @@ exports.saveLink = async (req, res) => {
     title: title,
     linkUrl: linkUrl,
   };
-  
-  console.log(link);
-  const linkSaved = await Link.create(link);
 
-  res.status(201).json({
-    message: "Link created with success",
-    data: linkSaved,
-  });
+  try {
+    const linkSaved = await Link.create(link);
+    res.status(201).json({
+      message: "Link created with success",
+      data: linkSaved,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "error trying to save the link in the db: " + err,
+    });
+  }
 };
