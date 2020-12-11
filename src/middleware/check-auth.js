f3dd2af8be14ed4ea46ea7186668ad8fb02dcc6e
Denis <dineresc@gmail.com>
2020-12-11 16:48:41 -0300

    remove console.log

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -4,7 +4,6 @@ require("dotenv/config");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
     jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (err) {
