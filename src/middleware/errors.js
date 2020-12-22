eb11797037039247765194969d413b40ee13311a
Denis <dineresc@gmail.com>
2020-12-21 23:47:57 -0300

    log do erro pra fazer test

--- src/middleware/errors.js
+++ src/middleware/errors.js
@@ -1,7 +1,7 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
-
+  console.log(err);
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
